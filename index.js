03cf8de6eabaed6822876de31ce4c39decd5beb3
canvaskisa <canvaskisa@gmail.com>
2016-06-14 17:55:48 +0300

    Add naive implementation of modules paths resolving.

--- index.js
+++ index.js
@@ -7,16 +7,37 @@ var match = require('posthtml-match-helper');
 
 module.exports = function plugin(options) {
 	options = options || {};
-	options.context = options.context || './';
+	options.ext = options.ext || '.html';
 	options.plugins = options.plugins || [];
+	options.context = path.resolve(options.context || './');
 
 	return function parse(tree) {
 		var promises = [];
 
 		tree.match(match('module[href]'), function (module) {
+			var name = path.parse(module.attrs.href).name;
+			var dir = path.parse(module.attrs.href).dir;
+			dir = dir === '.' ? '' : dir + '/';
+			var prev = tree.prevModuleHref ? path.parse(tree.prevModuleHref).name + '/' : '';
+
+			var lookups = [
+				path.resolve(options.context, (dir.charAt(0) === '/' ? '' : prev) + (dir.charAt(0) === '/' ? dir.substr(1) : dir) + name + options.ext),
+				path.resolve(options.context, (dir.charAt(0) === '/' ? '' : prev) + (dir.charAt(0) === '/' ? dir.substr(1) : dir) + name + '/index' + options.ext)
+			];
+
 			promises.push(new Promise(function (resolve, reject) {
-				return fs.readFile(path.resolve(options.context, module.attrs.href), 'utf8', function (err, res) {
-					return err ? reject(err) : resolve(res);
+				return Promise.all(lookups.map(function (path) {
+					return new Promise(function (resolve) {
+						return fs.readFile(path, 'utf8', function (err, res) {
+							return resolve(err || res);
+						});
+					});
+				})).then(function (res) {
+					var result = res.reduce(function (prev, curr) {
+						return prev instanceof Error ? curr : prev;
+					});
+
+					return result instanceof Error ? reject(result) : resolve(result);
 				});
 			}).then(function (content) {
 				return posthtml([function (tree) {
@@ -32,6 +53,7 @@ module.exports = function plugin(options) {
 					return tree;
 				}).process(content);
 			}).then(function (processed) {
+				processed.tree.prevModuleHref = module.attrs.href;
 				return parse(processed.tree);
 			}).then(function (content) {
 				module.tag = false;
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import test from 'ava';
 import posthtml from 'posthtml';
 import plugin from '..';
@@ -19,4 +20,10 @@ test('Must replace <content/> with module\'s content', async t => {
 	const expected = `<div><button type="button">ButtonTest</button>\n</div>`;
 	const {html} = await posthtml().use(plugin()).process(actual);
 	t.is(html, expected);
+});
+
+test('Must resolve href\'s correctly', async t => {
+	const actual = '<div class="container"><module href="./header"></module></div>'
+	const {html} = await posthtml().use(plugin({context: path.resolve(__dirname, 'tree.spec')})).process(actual);
+	console.log(html);
 });
