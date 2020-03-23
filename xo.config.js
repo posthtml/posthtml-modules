05f8374e233e0d57e1c3074bc66ffc165e001d7d
Cosmin Popovici <hellocosmin@gmail.com>
2020-03-23 14:01:04 +0200

    Update xo config.

--- xo.config.js
+++ xo.config.js
@@ -2,6 +2,8 @@ module.exports = {
   space: true,
   rules: {
     'capitalized-comments': 0,
-    quotes: ['error', 'single', {allowTemplateLiterals: true}]
+    quotes: ['error', 'single', {allowTemplateLiterals: true}],
+    'promise/prefer-await-to-then': 0,
+    'prefer-object-spread': 0
   }
 };
